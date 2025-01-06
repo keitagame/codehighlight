fe1c2344fd7b8d2099ef57d70c8ab5e94268e499
keita <keitagame0319@icloud.com>
2025-01-06 09:17:07 +0900

    Update codehighlight.js
    変数の型のハイライトを追加しました

--- codehighlight.js
+++ codehighlight.js
@@ -2,14 +2,18 @@ function highlight(code){
   
   const patterns = {
     keyword: /\b(function|if|else|for)\b/g,
-   
+    const: /\b(const)\b/g,
+    let: /\b(let)\b/g,
+    var: /\b(var)\b/g,
     comment: /(\/\/.*?$|\/\*[\s\S]*?\*\/)/gm
   };
 
  
   let highlightedCode = code.textContent
     .replace(patterns.keyword, '<span class="keyword" style ="color: blue;">$&</span>')
-   
+    .replace(patterns.const, '<span style ="color: orange;">$&</span>')
+   .replace(patterns.let, '<span style ="color: red;">$&</span>')
+    .replace(patterns.var, '<span style ="color: green;">$&</span>')
     .replace(patterns.comment, '<span class="comment" style ="color: gray;">$&</span>');
 
   code.innerHTML = highlightedCode;
