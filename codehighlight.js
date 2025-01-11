224c634cf8a76377fb64d752e2507100ec0eabbd
keita <keitagame0319@icloud.com>
2025-01-11 22:55:58 +0900

    Update codehighlight.js

--- codehighlight.js
+++ codehighlight.js
@@ -1,36 +1,36 @@
 function highlight(code){
   
   const patterns = {
-   name: /\b[a-zA-Z_]/g,
-   be: /\b[a-zA-Z_]\w*(?=\s*=\s*)/g,
-    beforeDot: /\b[a-zA-Z_]\w*(?=\.)/g, 
-   afDot: /(?<=\.)\b[a-zA-Z_]\w*\b/g,
-     d: /\b[a-zA-Z_]\w*\s+(?=\:)/g, 
-    string: /(".*?"|'.*?'|`.*?`)/g,
-    keyword: /\b(function|console|return|async|if|else|await)\b/g,
+   num: /(?!.*span)\d+/g,
+  mot: /[a-zA-Z_]\w*(?=\:)/g,
+    string: /(?!.*span)(".*?"|'.*?'|`.*?`)/g,
+    keyword: /\b(function|console|return|async|if|else|await|document)\b/g,
     const: /\b(const)\b/g,
     let: /\b(let)\b/g,
     var: /\b(var)\b/g,
-   functions: /\b[a-zA-Z_]\w*(?=\s*\()/g, // 関数名の正規表現
-    r: /\b(var|let|const)\s+([a-zA-Z_]\w*)\b/g,
+   functions: /\b[a-zA-Z_]\w*(?=\s*\()/g, 
+   
+  
     comment: /(\/\/.*?$|\/\*[\s\S]*?\*\/)/gm
   };
 let variableNames = [];
  
   let highlightedCode = code.textContent
-   
+  .replace(patterns.mot, '<span style ="color: #00f00f;">$&</span>')
+    .replace(patterns.num, '<span style ="color: #00f00f;">$&</span>')
+
     .replace(patterns.string, '<span style ="color: #ff00ff;">$&</span>')
    .replace(patterns.functions, '<span style ="color: #c000ff;">$&</span>')
-   .replace(patterns.r, '<span style ="color: #c0ff00;">$&</span>')
+ 
     .replace(patterns.keyword, '<span class="keyword" style ="color: #00c0ff;">$&</span>')
     .replace(patterns.const, '<span style ="color: orange;">$&</span>')
    .replace(patterns.let, '<span style ="color: red;">$&</span>')
-     .replace(patterns.beforeDot, '<span style ="color: red;">$&</span>')
     
     .replace(patterns.var, '<span style ="color: green;">$&</span>')
      .replace(patterns.d, '<span style ="color: blue;">$&</span>')
-    .replace(patterns.afDot, '<span style ="color: #00ff00;">$&</span>')
+ 
     .replace(patterns.comment, '<span class="comment" style ="color: gray;">$&</span>');
+ 
   console.log(variableNames);
   code.innerHTML = highlightedCode;
 }
