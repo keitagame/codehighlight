66b9495e71cba6b90f1731bef8ebe66411f249da
keita <keitagame0319@icloud.com>
2025-01-13 15:29:09 +0900

    Update codehighlight.js

--- codehighlight.js
+++ codehighlight.js
@@ -1,7 +1,7 @@
 function highlight(code){
   
   const patterns = {
-  num: />\s*(\d+?)\s*</g,
+  num: /\b\d+\b/g,
   mot: /[a-zA-Z_]\w*(?=\:)/g,
     string: /(?!.*span)(".*?"|'.*?'|`.*?`)/g,
     keyword: /\b(function|console|return|if|else)\b/g,
@@ -19,7 +19,7 @@ let variableNames = [];
   let highlightedCode =code.textContent
 
 .replace(patterns.mot, '<span style ="color: #00f00f;">$&</span>')
-
+ .replace(patterns.num, '<span style ="color: #00ff00;">$&</span>')
     .replace(patterns.k, '<span style ="color: #00ff00;">$&</span>')
     .replace(patterns.string, '<span style ="color: #ff00ff;">$&</span>')
    .replace(patterns.functions, '<span style ="color: #c000ff;">$&</span>')
